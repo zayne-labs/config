b860d3fcd924c99a87418819a5cd3903d1ed516a
Ryan-Zayne <phoenixastro97@gmail.com>
2025-03-21 15:58:46 +0100

    fix: removal of valid options in prettier config

--- packages/prettier-config/src/configs.ts
+++ packages/prettier-config/src/configs.ts
@@ -1,7 +1,7 @@
-import { type AnyString, defineEnum } from "@zayne-labs/toolkit-type-helpers";
+import type { AnyString } from "@zayne-labs/toolkit-type-helpers";
 import type { Config } from "prettier";
 
-export const baseConfig = defineEnum({
+export const baseConfig = {
 	experimentalOperatorPosition: "start",
 	jsxSingleQuote: false,
 	printWidth: 107,
@@ -9,9 +9,12 @@ export const baseConfig = defineEnum({
 	tabWidth: 3,
 	trailingComma: "es5",
 	useTabs: true,
-}) satisfies Config;
+} as const satisfies Config;
 
 export type ConfigWithTailwind = Omit<Config, "plugins"> & {
+	customAttributes?: string[];
+	customFunctions?: string[];
+	endPosition?: "absolute-with-indent" | "absolute" | "relative";
 	plugins?: Array<
 		// eslint-disable-next-line perfectionist/sort-union-types -- prettier-plugin-tailwindcss should come before prettier-plugin-classnames
 		"prettier-plugin-tailwindcss" | "prettier-plugin-classnames" | "prettier-plugin-merge" | AnyString
@@ -24,11 +27,20 @@ export type ConfigWithTailwind = Omit<Config, "plugins"> & {
 	tailwindStylesheet?: `./${string}`;
 };
 
-export const configWithTailwind = defineEnum({
+/**
+ * @description Prettier configuration with Tailwind CSS support.
+ *
+ * @docs
+ *  - [prettier-plugin-tailwindcss](https://github.com/tailwindlabs/prettier-plugin-tailwindcss)
+ *  - [prettier-plugin-classnames](https://github.com/ony3000/prettier-plugin-classnames)
+ */
+export const configWithTailwind = {
 	...baseConfig,
+	customAttributes: ["classNames", "classes"],
+	customFunctions: ["cnMerge", "cnJoin", "cn", "tv"],
 	endingPosition: "absolute-with-indent",
 	plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-classnames", "prettier-plugin-merge"],
 	tailwindAttributes: ["classNames", "classes"],
 	tailwindFunctions: ["cnMerge", "cnJoin", "cn", "tv"],
 	tailwindStylesheet: "./tailwind.css",
-} satisfies ConfigWithTailwind);
+} as const satisfies ConfigWithTailwind;
